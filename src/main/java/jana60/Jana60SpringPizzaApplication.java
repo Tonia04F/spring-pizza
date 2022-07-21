3cde712b9b207393954454aba3d2b30f7305a3f5
tonia <tonia@cia.como>
2022-07-21 22:32:09 +0200

    fine consegna base

---
+++ src/main/java/jana60/Jana60SpringPizzaApplication.java
@@ -0,0 +1,13 @@
+package jana60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jana60SpringPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jana60SpringPizzaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jana60/controller/PizzaController.java
@@ -0,0 +1,31 @@
+package jana60.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jana60.model.Pizza;
+import jana60.repository.PizzaRepository;
+
+@Controller
+@RequestMapping("/pizza")
+public class PizzaController {
+	
+	@Autowired
+	private PizzaRepository repo;
+	
+	
+	@GetMapping 
+	public String listaPizze(Model model){
+		List<Pizza> lPizza = (List<Pizza>)repo.findAll();
+		model.addAttribute("listaPizze", lPizza);
+		return "pizza";
+	}
+	
+	
+	
+}
