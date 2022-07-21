e8aa5a198dc30d56586464fca55ffa5ad6a29051
tonia <tonia@cia.como>
2022-07-22 00:45:09 +0200

    fine consegna base

--- src/main/java/jana60/controller/PizzaController.java
+++ src/main/java/jana60/controller/PizzaController.java
@@ -2,10 +2,15 @@ package jana60.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jana60.model.Pizza;
@@ -26,6 +31,8 @@ public class PizzaController {
 		return "pizza";
 	}
 	
+	
+	//crea nuovo ogetto pizza vuoto da aggiungere nella tramite la form
 	@GetMapping("/add")
 	//creo metodo per funzionamento della form, bottone
 	public String creaPizza(Model model) {
@@ -33,4 +40,16 @@ public class PizzaController {
 		return "crea";
 	}
 	
+	  @PostMapping("/add")
+	  public String salva(@Valid @ModelAttribute("creaPizza") Pizza formPizza, BindingResult br) {
+	    // testo se ci sono errori di validazione
+	    if (br.hasErrors()) {
+	      // se ci sono errori non salvo il book su database ma ritorno alla form precaricata
+	      return "crea";
+	    } else {
+	      // se non ci sono errori salvo il book che arriva dalla form
+	      repo.save(formPizza);
+	      return "redirect:/"; // non cercare un template, ma fai la HTTP redirect a quel path
+	    }
+	  }
 }
