679ace53a36b61cd09130da03262df54a2ca3393
tonia <tonia@cia.como>
2022-07-21 23:14:43 +0200

    funziona

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizza(descrizione, nome, prezzo)VALUES('Pizza tradizionale dal sapore mediterraneo con pomodoro, mozzarella e basilico fresco', 'Margherita', 5);
+
+INSERT INTO pizza(descrizione, nome, prezzo)VALUES('Pizza scelta per bambini, soddisfacente per tutti i gustu con provola, wrustel e patatine fritte(fresche)', 'Pizza baby', 5);
